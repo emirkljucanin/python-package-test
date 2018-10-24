06c17b04dae175517bdedd7b32dd851f1d14d2bf
Emir Kljucanin <emir@virgilcareers.com>
2018-10-24 22:17:03 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='myPackageExample',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Moj paket',
